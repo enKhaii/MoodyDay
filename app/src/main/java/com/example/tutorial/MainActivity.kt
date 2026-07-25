1945c96b596235bc3dc3088763c47a05b513c57d
enKhaii <khai05227@gmail.com>
2026-07-25 17:12:06 +0800

    Initial commit

---
+++ app/src/main/java/com/example/tutorial/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.tutorial
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.tutorial.ui.theme.TutorialTheme
+import com.example.tutorial.ui.theme.Typography
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        installSplashScreen()
+        setContent {
+            TutorialTheme {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Greeting("")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Composable
+fun CustomText(text: String){
+    Text(
+        text = text,
+        style = Typography.bodyMedium
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TutorialTheme {
+        Column() {
+            Greeting("Android")
+            CustomText(text = "Hello World")
+        }
+
+    }
+}
